59954803278f24216f4b3508e674bcb515b4cca2
lolita89 <cika.lolita@hotmail.com>
2018-05-25 13:55:28 +0200

    basic web done

---
+++ src/components/CustomNavBar.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import {Navbar, Nav, NavItem } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './CustomNavbar.css';
+
+class CustomNavBar extends Component {
+    render() {
+        return (
+         <Navbar default collapseOnSelect>
+         <Navbar.Header>
+             <Navbar.Brand>
+                 <Link to="/">MyStory</Link>
+             </Navbar.Brand>
+            <Navbar.Toggle />
+         </Navbar.Header>
+        <Navbar.Collapse>
+         <Nav pullRight>
+            <NavItem eventKey={1} componentClass={Link} to ="/">
+              Home
+            </NavItem>
+            <NavItem eventKey={2} componentClass={Link} to ="/about">
+              About
+            </NavItem>
+            <NavItem eventKey={3} componentClass={Link} to ="/news">
+              News
+            </NavItem>
+            </Nav>
+            </Navbar.Collapse>
+         </Navbar>
+        );
+    }
+}
+
+export default CustomNavBar;
