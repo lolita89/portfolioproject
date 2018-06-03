211092574d9125300b19212641ba4b2c9a1a7212
lolita89 <cika.lolita@hotmail.com>
2018-06-03 15:58:58 +0200

    changed background

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -2,17 +2,31 @@ import React, { Component } from 'react';
 import { Image } from 'react-bootstrap';
 import './Quotes.css';
 
-class Quotes extends Component {
+
+class Quotes extends React.Component {
+    // handleChange(e){
+    //     const title = e.target.value
+    //     this.props.changeTitle(title);
+
+    // }
     render() {
         return (
+            <div>
             <div className="text-center">
                 <Image src="assets/woman-in-tech.jpg" className="header-image" />
                 <Image src="assets/woman-in-tech2.jpg" className="header-image" />
                 <Image src="assets/woman-in-tech3.jpg" className="header-image" />
                 <Image src="assets/woman-in-tech4.jpg" className="header-image " />
             </div>
+            <div>
+                {/* {this.props.title}
+                <input value={this.props.title} onChange={this.handleChange.bind(this)} /> */}
+                </div>
+            </div>
+            
         );
     }
 }
 
 export default Quotes;
+
